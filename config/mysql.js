99f5081c93a9639c870e1a10dd100481a8d63812
Sharon-Liu-go <69050005+Sharon-Liu-go@users.noreply.github.com>
2024-01-18 14:22:09 +0800

    Update mysql.js
    remove unnecessary info

--- config/mysql.js
+++ config/mysql.js
@@ -1,6 +1,6 @@
 module.exports = {
-    host: process.env.MYSQL_HOST || '34.150.115.117',
-    user: process.env.MYSQL_USER || 'root',
-    password: process.env.MYSQL_PASSWORD || 'I4p;122ihga"kb4]v',
-    port: process.env.MYSQL_PORT || 3306
+    host: process.env.MYSQL_HOST ,
+    user: process.env.MYSQL_USER ,
+    password: process.env.MYSQL_PASSWORD ,
+    port: process.env.MYSQL_PORT
 }
