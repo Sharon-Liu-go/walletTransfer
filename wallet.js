c96cb415cdab4e9fd2f196fa8fba989289b7e6c1
Sharon-liu-go <innovate72095@gmail.com>
2024-01-21 22:38:46 +0800

    feat: add walletTransfer_preset_check_duplicate

--- wallet.js
+++ wallet.js
@@ -1,6 +1,10 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
-const walletTransfer_preset_check = require('./class/walletTransfer_preset_check');
+const walletTransfer_preset_check_duplicate = require('./class/walletTransfer_preset_check');
 const checkRepeatedAccounts = new walletTransfer_preset_check;
-checkRepeatedAccounts.exec();
+await checkRepeatedAccounts.exec();
+
+const walletTransfer_preset_check_duplicate = require('./class/walletTransfer_preset_check_duplicate');
+const checkDuplicateAccounts = new walletTransfer_preset_check_duplicate;
+await checkDuplicateAccounts.exec();
